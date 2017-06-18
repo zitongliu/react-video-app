2768ebe040c324c3435a9b8f3831c64aa3961219
zitongliu <zitongliu7@gmail.com>
2017-06-18 14:12:24 +1000

    videos wip

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyBPPgRmVoR_2FQc8OFVH0qg8P27H53MBJo';
 
 // Create a new componet. This component shouldproduce some HTML
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // this.setState({ videos: videos });
+      this.setState({videos});
+    })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 };
 
 // Take this component's generated HTML and put in on the page (in the DOM)
